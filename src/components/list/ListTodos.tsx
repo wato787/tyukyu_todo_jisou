9e99d6669a28cd5eabed64c2fedb0656a8afa492
wato787 <wato787@icloud.com>
2023-02-26 03:49:13 +0900

    編集で入力した値が反映されない

--- src/components/list/ListTodos.tsx
+++ src/components/list/ListTodos.tsx
@@ -1,6 +1,6 @@
-import { Button, Flex, list, ListItem, UnorderedList } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { useRecoilState } from "recoil";
+import { Button, Flex, ListItem, UnorderedList } from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState} from "recoil";
 import { todoListState } from "../../recoil/Atom";
 import { todoli } from "../../types/todoli";
 
@@ -42,11 +42,11 @@ export const ListTodos = () => {
   const handleEdit = (id: string, value: string) => {
     const newTodos = listTodo.map((todo) => {
       if (todo.id === id) {
-        todo.text = value;
+        Object.assign(todo.text,{value})
+        // todo.text = value;
       }
       return todo;
     });
-    // todos ステートを更新
     setListTodo(newTodos);
   };
 
@@ -56,50 +56,61 @@ export const ListTodos = () => {
     const newListTodos = list.map((todo) =>
       id === todo.id
         ? {
-          ...todo,
-          isEdit: !todo.isEdit
-        }
+            ...todo,
+            isEdit: !todo.isEdit,
+          }
         : todo
     );
     setListTodo(newListTodos);
-  }
+  };
 
   return (
     <Flex justifyContent="center">
       <UnorderedList>
         {listTodo.map((todo) => (
           <>
-          {/* 3. todo.isEditがtrueならばtodoの内容ではなく、編集フォームを表示させ、作成したhandleEditで編集機能を持たせればよいと思います！ */}
-          {todo.isEdit ? (
-            <ListItem key={todo.id} fontSize="20px">
-              編集フォームの作成
+            {/* 3. todo.isEditがtrueならばtodoの内容ではなく、編集フォームを表示させ、作成したhandleEditで編集機能を持たせればよいと思います！ */}
+            {todo.isEdit ? (
+              <ListItem key={todo.id} fontSize="20px">
+                {/* 編集フォームの作成 */}
+                <input
+                  type="text"
+                  value={todo.text}
+                  onChange={(e) => handleEdit(todo.id, e.target.value)}
+                />
                 <Button onClick={() => toggleEditForm(todo.id)}>戻る</Button>
-            </ListItem>
-          ) : (
-            <ListItem key={todo.id} fontSize="20px">
-              <input
-                type="text"
-                value={todo.text}
-                onChange={(e) => handleEdit(todo.id, e.target.value)}
-              />
+              </ListItem>
+            ) : (
+              <ListItem key={todo.id} fontSize="20px">
+                {todo.text}
 
-              <Button
-                onClick={() => onClickStatusChange(todo.id)}
-                colorScheme="cyan"
-                m={2}
-              >
-                {todo.status}
-              </Button>
-              <Button m={2} onClick={() => onClickDelete(todo.id)} isDisabled={todo.status === "着手"}>
-                削除
-              </Button>
-              <Button m={2} onClick={() => onClickComplete(todo.id)} isDisabled={todo.status === "未着手"}>
-                完了
-              </Button>
-              {/* 2．編集ボタンが押されたときにtodo.isEditを切り替えてtrueにする */}
-                  <Button m={2} onClick={() => toggleEditForm(todo.id)}>編集</Button>
-            </ListItem>
-          )}
+                <Button
+                  onClick={() => onClickStatusChange(todo.id)}
+                  colorScheme="cyan"
+                  m={2}
+                >
+                  {todo.status}
+                </Button>
+                <Button
+                  m={2}
+                  onClick={() => onClickDelete(todo.id)}
+                  isDisabled={todo.status === "着手"}
+                >
+                  削除
+                </Button>
+                <Button
+                  m={2}
+                  onClick={() => onClickComplete(todo.id)}
+                  isDisabled={todo.status === "未着手"}
+                >
+                  完了
+                </Button>
+                {/* 2．編集ボタンが押されたときにtodo.isEditを切り替えてtrueにする */}
+                <Button m={2} onClick={() => toggleEditForm(todo.id)}>
+                  編集
+                </Button>
+              </ListItem>
+            )}
           </>
         ))}
       </UnorderedList>
